e279c9598918206843a62728fb6f015b500856b8
ShengCN <451718145@qq.com>
2020-12-10 04:41:31 -0500

    finish SH blog

--- new_website/images/make_cirle.py
+++ new_website/images/make_cirle.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-img = plt.imread("profile.jpg").copy()
+img = plt.imread("profile_light.jpg").copy()
 if img.dtype == np.uint8:
     img = img/255.0
 
@@ -14,7 +14,7 @@ for i in tqdm(range(h)):
     for j in range(w):
         cur_pos = np.array([i,j])
         dis = cur_pos - center
-        if np.dot(dis, dis) > rad * rad: 
+        if np.dot(dis, dis) > rad * rad:
             img[i,j,:] = 1.0
 
 img = img[center[0]-rad:center[0]+rad,center[1]-rad:center[1]+rad,:]
